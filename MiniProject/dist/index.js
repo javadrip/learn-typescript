c7bc62e7fac53a5e2f4d700a6bc1d805c3651e08
Vinleon Ang <javadripdev@gmail.com>
2023-04-11 14:27:44 +0800

    Add form

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log("Good night!");
-console.log("npm start configured!");
+var btn = document.getElementById("btn");
+var input = document.getElementById("todoinput");
+var form = document.querySelector("form"); // We can use "form" instead of the form id "#todoform" because there's only one form on the page. By default "form" selects the first form
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submitted!");
+}
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", function () {
+//   alert("Clicked!");
+// });
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,2 +1,14 @@
-console.log("Good night!");
-console.log("npm start configured!");
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!; // We can use "form" instead of the form id "#todoform" because there's only one form on the page. By default "form" selects the first form
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  console.log("Submitted!");
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// btn.addEventListener("click", function () {
+//   alert("Clicked!");
+// });
