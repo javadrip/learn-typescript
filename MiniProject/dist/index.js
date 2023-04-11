81e336a532a04295743fda8dbb3b7081f80d9802
Vinleon Ang <javadripdev@gmail.com>
2023-04-11 16:55:27 +0800

    Add checkbox persistence

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -11,6 +11,9 @@ function readTodos() {
         return [];
     return JSON.parse(todosJSON);
 }
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
 function handleSubmit(e) {
     e.preventDefault();
     var newTodo = {
@@ -19,13 +22,18 @@ function handleSubmit(e) {
     };
     createTodo(newTodo);
     todos.push(newTodo);
-    localStorage.setItem("todos", JSON.stringify(todos));
+    saveTodos();
     input.value = "";
 }
 function createTodo(todo) {
     var newLI = document.createElement("li");
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
     newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -17,6 +17,10 @@ function readTodos(): Todo[] {
   return JSON.parse(todosJSON);
 }
 
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
 
@@ -28,7 +32,7 @@ function handleSubmit(e: SubmitEvent) {
   createTodo(newTodo);
   todos.push(newTodo);
 
-  localStorage.setItem("todos", JSON.stringify(todos));
+  saveTodos();
   input.value = "";
 }
 
@@ -36,6 +40,12 @@ function createTodo(todo: Todo) {
   const newLI = document.createElement("li");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+
+  checkbox.addEventListener("change", function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
 
   newLI.append(todo.text);
   newLI.append(checkbox);
