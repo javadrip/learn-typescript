ea95470792cd4591f6143b10d48c955cb020308d
Vinleon Ang <javadripdev@gmail.com>
2023-04-11 14:33:44 +0800

    Handle form submission and display input

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,10 +1,15 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!; // We can use "form" instead of the form id "#todoform" because there's only one form on the page. By default "form" selects the first form
+const list = document.getElementById("todolist")!;
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log("Submitted!");
+  const newToDoText = input.value;
+  const newLI = document.createElement("LI");
+  newLI.append(newToDoText);
+  list.append(newLI);
+  input.value = "";
 }
 
 form.addEventListener("submit", handleSubmit);
